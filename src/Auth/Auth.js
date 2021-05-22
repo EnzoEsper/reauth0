642c3f35458bdff4e46565105145820290cf2539
enzoesper <enzoesper@hotmail.com>
2021-05-22 18:29:13 -0300

    implementing the handle authentication function inside the Auth.js file for handle authentication inside the components that requires it

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,4 +1,4 @@
-import auth0 from 'auth0-js';
+import auth0 from "auth0-js";
 
 export default class Auth {
   constructor(history) {
@@ -8,11 +8,34 @@ export default class Auth {
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
       redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
       responseType: "token id_token", // specifies the response type that we want to get back when a user is authenticated.
-      scope: "openid profile email"
+      scope: "openid profile email",
     });
   }
 
   login = () => {
     this.auth0.authorize();
+  };
+
+  handleAuthentication = () => {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+        this.history.push("/");
+      } else if (err) {
+        this.history.push("/");
+        alert(`Error: ${err.error}. Check the console for further details.`);
+        console.log(err);
+      }
+    })
+  };
+
+  setSession = authResult => {
+    console.log(authResult);
+    // set the time that the acces token will expire
+    const expiresAt = JSON.stringify(authResult.expiresIn * 1000 + new Date().getTime());
+    
+    localStorage.setItem("acces_token", authResult.accessToken);
+    localStorage.setItem("id_token", authResult.idToken);
+    localStorage.setItem("expires_at", expiresAt);
   }
 }
